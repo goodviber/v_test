2179ba9ae14fd26a73ca6f3015e06c82285b439e
maxmills <8balldigitalsolutions@gmail.com>
2018-06-11 01:37:53 +0100

    product specs

---
+++ spec/product_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe Product do
+  subject { Product.new('001', 'cheap chair', 9.25) }
+
+  it 'should return correct product code' do
+    expect(subject.code).to eq('001')
+  end
+
+  it 'should return correct product name' do
+    expect(subject.name).to eq('cheap chair')
+  end
+
+  it 'should return correct price' do
+    expect(subject.price).to eq(9.25)
+  end
+end
--- spec/promo_rule_spec.rb
+++ spec/promo_rule_spec.rb
@@ -9,5 +9,44 @@ describe PromoRule do
     end
   end
 
+  describe 'discount_promo_rule method' do
+   subject { PromoRule.discount_rule(60, 10) }
+
+   it 'should be instance of PromoRule' do
+     expect(subject).to be_a_kind_of(PromoRule)
+   end
+ end
+
+ describe 'creating discount_promo_rule processor' do
+    it 'should call block with no arguments' do
+      value = false
+      promo_rule = PromoRule.new(PromoRule::TYPE[:discount]) do
+        value = true
+      end
+      expect(promo_rule.apply).to be_truthy
+    end
+
+    it 'should not call block with one argument' do
+      promo_rule = PromoRule.new(PromoRule::TYPE[:discount]) do |value|
+        value
+      end
+      expect(promo_rule.apply('foo')).not_to eq('foo')
+    end
+
+    it 'should call block with two arguments' do
+      promo_rule = PromoRule.new(PromoRule::TYPE[:discount]) do |value1, value2|
+        "#{value1}-#{value2}"
+      end
+      expect(promo_rule.apply('foo', 'bar')).to eq('foo-bar')
+    end
+
+    it 'should not call block with three arguments' do
+      promo_rule = PromoRule.new(PromoRule::TYPE[:discount]) do |value1, value2, value3|
+        "#{value1}-#{value2}-#{value3}"
+      end
+      expect(promo_rule.apply('foo', 'bar', 'baz')).to eq('foo-bar-baz')
+    end
+
+  end
 
 end
